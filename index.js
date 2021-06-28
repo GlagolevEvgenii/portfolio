998c599ca73ca75aa769f31810e17bdd3322ddd3
evgenii <e.glagolev@artjoker.ua>
2021-06-28 06:09:05 +0300

    add planeMesh add dut gui,start hover effect

--- index.js
+++ index.js
@@ -1,13 +1,126 @@
 import * as THREE from 'three';
+import * as dat from 'dat.gui'
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
+
+
+//gui
+const gui = new dat.GUI()
+const world = {
+  plane: {
+    width: 5,
+    height: 5,
+    widthSegments: 10,
+    heightSegments: 10
+  }
+}
+gui.add(world.plane, 'width', 1, 20).onChange(generatePlane)
+gui.add(world.plane, 'height', 1, 20).onChange(generatePlane)
+gui.add(world.plane, 'widthSegments', 1, 20).onChange(generatePlane)
+gui.add(world.plane, 'heightSegments', 1, 20).onChange(generatePlane)
+
+function generatePlane() {
+  planeMesh.geometry.dispose()
+  planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, world.plane.widthSegments, world.plane.heightSegments)
+  const {array} = planeMesh.geometry.attributes.position
+  for (let i = 0; i < array.length; i += 3) {
+    const x = array[i]
+    const y = array[i + 1]
+    const z = array[i + 2]
+    array[i + 2] = z + Math.random()
+  }
+}
+
+const raycaster = new THREE.Raycaster()
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75,innerWidth/innerHeight,0.1,1000)
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGL1Renderer()
 
 
-renderer.setSize(innerWidth,innerHeight)
+renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
-const boxGeometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({color:0x00ff})
-const mesh = new THREE.Mesh(boxGeometry,material)
-scene.add(mesh)
-renderer.render(scene,camera)
+new OrbitControls(camera, renderer.domElement)
+camera.position.z = 5
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
+const planeMaterial = new THREE
+    .MeshPhongMaterial({
+      side: THREE.DoubleSide,
+      flatShading: THREE.FlatShading,
+      vertexColors: true
+    })
+
+const planeMesh = new THREE.Mesh(
+    planeGeometry, planeMaterial)
+scene.add(planeMesh)
+console.log(planeMesh.geometry.attributes.position.array)
+
+const {array} = planeMesh.geometry.attributes.position
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i]
+  const y = array[i + 1]
+  const z = array[i + 2]
+
+  array[i + 2] = z + Math.random()
+}
+
+const colors = []
+for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+  colors.push(0, 0.19, 0.4)
+
+}
+planeMesh.geometry.setAttribute(
+    'color', new THREE.BufferAttribute(
+        new Float32Array(colors), 3))
+
+//light
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(0, 0, 1)
+scene.add(light)
+const backLight = new THREE.DirectionalLight(0xffffff, 1)
+backLight.position.set(0, 0, -1)
+scene.add(backLight)
+
+const mouse = {
+  x: undefined,
+  y: undefined
+}
+
+function animate() {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+
+
+  raycaster.setFromCamera(mouse, camera)
+  const intersects = raycaster.
+    intersectObject(planeMesh)
+  if (intersects.length > 0) {
+    const {color} = intersects[0].object.geometry.attributes
+    //vertice 1
+    color.setX(intersects[0].face.a,0.1)
+    color.setY(intersects[0].face.a,0.5)
+    color.setZ(intersects[0].face.a,1)
+
+    //vertice 2
+    color.setX(intersects[0].face.b,0.1)
+    color.setY(intersects[0].face.b,0.5)
+    color.setZ(intersects[0].face.b,1)
+
+    //vertice 3
+    color.setX(intersects[0].face.c,0.1)
+    color.setY(intersects[0].face.c,0.5)
+    color.setZ(intersects[0].face.c,1)
+
+    intersects[0].object.geometry.attributes.color.needsUpdate = true
+
+    console.log(intersects[0].face)
+  }
+}
+
+animate()
+
+addEventListener("mousemove", (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1
+})
+
