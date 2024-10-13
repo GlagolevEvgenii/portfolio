a029ef2ce4ea2ff11b3cf045dcba1c2744247f75
IevgeniiGlagoliev <glagolev.dev@gmail.com>
2024-10-14 00:56:46 +0300

    rewrite horizontal scroll

--- js/main.js
+++ js/main.js
@@ -5,62 +5,17 @@ const embedEngine = {
     },
     binds() {
 
-        const blocks = document.getElementsByClassName("container__item");
-        const container = document.getElementsByClassName("container");
-
-        if(blocks.length){
-            const hs = new HorizontalScroll.default({
-                blocks: blocks,
-                container: container,
-                isAnimated: true,
-                springEffect: 0.8,
-            });
-        }
-
-
-        // smooth scroll
+        gsap.registerPlugin(ScrollTrigger);
         const scroll = new LocomotiveScroll({
-            el: document.querySelector('#js-scroll'),
+            el: document.querySelector("[data-scroll-container]"),
             smooth: true,
-            smoothMobile: true,
-            inertia: 0.75
+            direction: "horizontal",
+            lerp: 0.08,
+            getDirection: true,
+            smoothMobile: false,
+            scrollFromAnywhere: false,
         });
-
-        // div following mouse
-        TweenMax.from(".cursor-follow", 2, {
-            delay: 1,
-            opacity: 0
-        });
-
-        follower = document.querySelector('.cursor-follow');
-
-        posX = 0;
-        posY = 0;
-        mouseX = 0;
-        mouseY = 0;
-
-        TweenMax.to({}, 0.016, {
-            repeat: -1,
-            onRepeat: function () {
-                posX += (mouseX - posX) / 9;
-                posY += (mouseY - posY) / 9;
-
-                TweenMax.set(follower, {
-                    css: {
-                        left: posX - 40,
-                        top: posY - 40,
-                    }
-                });
-            }
-        });
-
-        document.addEventListener("mousemove", function (e) {
-            mouseX = e.pageX;
-            mouseY = e.pageY;
-        });
-
-
-
+        scroll.on("scroll", ScrollTrigger.update);
 
 
     },
